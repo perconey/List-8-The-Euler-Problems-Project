793995c728ab7bfe7b5621239a55814ca4376a24
Michał Minikowski <perconey@gmail.com>
2017-11-06 22:38:59 +0100

    1st Problem

---
+++ P1 - Multiples of 3 and 5/P1 - Multiples of 3 and 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1___Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pool = 0;
+            int i;
+            for(i = 1; i<1000; i++)
+            {
+                if(i%3 == 0 || i%5 == 0)
+                {
+                    pool += i;
+                }
+            }
+
+            Console.WriteLine(pool);
+            Console.ReadKey();
+        }
+    }
+}
