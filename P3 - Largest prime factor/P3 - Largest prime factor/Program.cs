b1c2c430a0c1545be9a8160257d98ed4867f9d59
Perconey <perconey@gmail.com>
2017-11-16 20:51:55 +0100

    Problem 3

---
+++ P3 - Largest prime factor/P3 - Largest prime factor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3___Largest_prime_factor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            long number = 600851475143;
+            long updatedNumber = number;
+            long largestFact = 0;
+
+            int counter = 2;
+            while (counter * counter <= updatedNumber)
+            {
+                if (updatedNumber % counter == 0)
+                {
+                    updatedNumber = updatedNumber / counter;
+                    largestFact = counter;
+                }
+                else
+                {
+                    counter++;
+                }
+            }
+            if (updatedNumber > largestFact)
+            {
+                largestFact = updatedNumber;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
