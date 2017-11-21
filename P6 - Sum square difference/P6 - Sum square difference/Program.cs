88a79c64819c82e6dd5f192a8ddea4e31d326fd7
Perconey <perconey@gmail.com>
2017-11-21 23:48:29 +0100

    Problem 6

---
+++ P6 - Sum square difference/P6 - Sum square difference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P6___Sum_square_difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sumOfSquares = 0;
+            long squareOfSum = 0;
+
+            int n = 100;
+            int a100 = 100;
+            sumOfSquares = n *(a100 + 1) / 2;
+
+            squareOfSum = (n *(n + 1) *(2 *n + 1)) /6;
+
+            Console.WriteLine(sumOfSquares * sumOfSquares - squareOfSum);
+            Console.ReadKey();
+        }
+    }
+}
